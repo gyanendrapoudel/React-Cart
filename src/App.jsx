1e40870fe47da966481fec889d8c05290804a0ed
Gyanendra <gyanen56@gmail.com>
2024-08-08 16:58:03 +1000

    adding up and dowm arrow functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import Navbar from './components/Navbar'
 import Items from './components/Items'
 import './App.css'
+import { useEffect } from 'react'
 function App() {
- 
+  const fetchApi = async ()=>{
+      const response = await fetch("")
+      const data = await response.json()
+      console.log(data)
+  }
+ useEffect(()=>{},[])
   return (
     <>
      <Navbar/>
--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import { FaArrowAltCircleUp } from 'react-icons/fa'
 import { FaArrowCircleDown } from 'react-icons/fa'
 const SingleItem = ({ name, img, id }) => {
+   const [count, setCount]=useState(1)
   return (
     <div className="single-item">
       <div className="item-description">
@@ -15,9 +17,9 @@ const SingleItem = ({ name, img, id }) => {
         </div>
       </div>
       <div className="number">
-        <FaArrowAltCircleUp className="icons" />
-        <div>1</div>
-        <FaArrowCircleDown className="icons" />
+        <FaArrowAltCircleUp className="icons" onClick={()=>setCount(count+1)} />
+        <div>{count}</div>
+        <FaArrowCircleDown className="icons" onClick={()=>setCount(count<1?0:count-1)}/>
       </div>
     </div>
   )
